77112a2c4c85da9b6b2c2bb88ae28b757a412f24
vulpes94 <rwr9857@gmail.com>
2024-12-01 19:40:53 +0900

    it doesn't need for me

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TouchableOpacity from "@/pages/TouchableOpacity";
+import SimpleTodo from "@/pages/SimpleTodo";
+
+export default function HomeScreen() {
+  return (
+    <>
+      {/* <TouchableOpacity /> */}
+      <SimpleTodo />
+    </>
+  );
+}
